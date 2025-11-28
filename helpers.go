b194525de84e4cc29aba8dc43addb0a18987e9eb
delroscol98 <collindelrosario98@gmail.com>
2025-11-28 20:53:39 +1100

    created a function to generate presigned URL

--- helpers.go
+++ helpers.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
 	"os/exec"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
 func getVideoAspectRatio(filepath string) (string, error) {
@@ -54,4 +58,18 @@ func processVideoForFastStart(filepath string) (string, error) {
 	}
 
 	return outputFilePath, nil
+}
+
+func generatePresignedURL(s3Client *s3.Client, bucket, key string, expireTime time.Duration) (string, error) {
+	presignedClient := s3.NewPresignClient(s3Client)
+	params := s3.GetObjectInput{
+		Bucket: &bucket,
+		Key:    &key,
+	}
+	presignedHTTPRequest, err := presignedClient.PresignGetObject(context.TODO(), &params, s3.WithPresignExpires(expireTime))
+	if err != nil {
+		return "", err
+	}
+
+	return presignedHTTPRequest.URL, nil
 }
